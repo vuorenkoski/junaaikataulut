97c1847497b119d152b057c4432efa023bde636a
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2019-09-02 20:13:39 +0300

    0.92 muutettu versionumero: haku gradle tiedosta

--- app/src/main/java/fi/vuorenkoski/junaaikataulut/Tietoja.java
+++ app/src/main/java/fi/vuorenkoski/junaaikataulut/Tietoja.java
@@ -3,6 +3,7 @@ package fi.vuorenkoski.junaaikataulut;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tietoja extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class Tietoja extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tietoja);
+        TextView versioTeksti = findViewById(R.id.versioView);
+        versioTeksti.setText(BuildConfig.APPLICATION_ID+"\nVersio: "+BuildConfig.VERSION_NAME+" "+BuildConfig.BUILD_TYPE);
     }
 }
